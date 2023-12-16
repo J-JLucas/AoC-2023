d552413066ad04e23637a150e5e1192061e6a412
JoSh <jlucas205@proton.me>
2023-12-16 12:03:10 -0700

    basic structure, need to think about regex

--- d02/d2p1.cpp
+++ d02/d2p1.cpp
@@ -2,7 +2,22 @@
 #include <string>
 #include <regex>
 
+const int red {12};
+const int blue {14};
+const int green {13};
+
 int main(int argc, char *argv[]) {
+
+    std::string line {};
+
+    while(getline(std::cin, line)) {
+	bool invalid {false};
+	int game {0};
+	/* for (each game) {
+	 *	
+	 * }
+	*/
+    }
 
 
     return 0;
