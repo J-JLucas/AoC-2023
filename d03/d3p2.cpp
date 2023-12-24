b2a175dfad09f10bcfcffba87f14f1c1ffd8bd8a
JoSh <jlucas205@proton.me>
2023-12-24 16:27:25 -0700

    cleaned up p1

--- d03/d3p2.cpp
+++ d03/d3p2.cpp
@@ -7,36 +7,62 @@ std::vector<std::string> grid {};   // "engine" grid diagram
 
 bool validate(std::regex_iterator<std::string::iterator> it, int lineNum) {
     // takes in a substring and checks if surrounded
-    // by a validating each character of substring referenced to the 'grid'
+    // by a validating character in the grid
+    std::cout << std::endl;
+    std::cout << grid[lineNum-1] << std::endl;
+    std::cout << grid[lineNum] << std::endl;
+    std::cout << grid[lineNum+1] << std::endl;
+    std::cout << it->str() << std::endl;
+    
     bool valid = false;
     int pos = it->position();
     for(int k = 0; k < it->length(); k++) {
+	std::cout << "char: "<< (it->str())[k] << std::endl;
+	std::cout << std::endl;
 	// check left
 	if(k == 0) {
+	    std::cout << "left: " << grid[lineNum][pos-1] << std::endl;
 	    if(grid[lineNum][pos-1] != '.') {valid = true;}
 	}
+	
 	// check top left diagonal
+	std::cout << "top left: " << grid[lineNum-1][pos-1] << std::endl;
 	if(grid[lineNum-1][pos-1] != '.') {valid = true;}
+	
 	// check top
+	std::cout << "top: " << grid[lineNum-1][pos] << std::endl;
 	if(grid[lineNum-1][pos] != '.') {valid = true;}	
+	
 	// check top right diagonal
+	std::cout << "top right: " << grid[lineNum-1][pos+1] << std::endl;
 	if(grid[lineNum-1][pos+1] != '.') {valid = true;}
+	
 	// check right	
 	if(k == it->length()-1) {
+	    std::cout << "right: " << grid[lineNum][pos+1] << std::endl;
 	    if(grid[lineNum][pos+1] != '.') {valid = true;}
 	}	 
+		
 	// check bottom right diagonal
+	std::cout << "bottom right: " << grid[lineNum+1][pos+1] << std::endl;
 	if(grid[lineNum+1][pos+1] != '.') {valid = true;}
+	
 	// check bottom
+	std::cout << "bottom: " << grid[lineNum+1][pos] << std::endl;
 	if(grid[lineNum+1][pos] != '.') {valid = true;}
+
 	// check bottom left diagonal	
+	std::cout << "bottom left: " << grid[lineNum+1][pos-1] << std::endl;
 	if(grid[lineNum+1][pos-1] != '.') {valid = true;}
+	std::cout << std::endl;
 	pos++;
     }
+    std::cout << valid << std::endl;
     return valid;
 }
 
 int main(int argc, char* argv[]) {
+    
     int sum = 0;
     std::string line{};
 
@@ -58,12 +84,17 @@ int main(int argc, char* argv[]) {
 	std::regex_iterator<std::string::iterator> iter (line.begin(), line.end(), e);
 	std::regex_iterator<std::string::iterator> end;
     
+	//std::cout << line << std::endl;
 	while (iter!=end) {
 	    bool valid = validate(iter, i);
 	    if(valid) {sum += stoi(iter->str()); std::cout << iter->str() << std::endl;}
 	    iter++;
 	}
     }
+    for (auto line : grid) {
+	std::cout << line << std::endl;
+    }
+
     std::cout << sum << std::endl;
     return 0;
 }
