7791163e94ff0e8e0f91f0186ed1a50de0e1527b
JoSh <jlucas205@proton.me>
2023-12-24 12:27:58 -0700

    day one regex matching all part numbers, next step to validate numbers and sum

--- d03/d3p1.cpp
+++ d03/d3p1.cpp
@@ -21,12 +21,21 @@ int main(int argc, char* argv[]) {
 	grid.push_back(line);
     }
     grid.push_back(padding);
+    
+    std::regex e ("(\\d)+");	//match numerical substrings
 
-    /*
-    for(const auto &str : grid) {
-       std::cout << str << std::endl;
+    for (int i = 1; i < grid.size()-1 ; i++) {
+	std::string line = grid[i];
+	std::regex_iterator<std::string::iterator> rit (line.begin(), line.end(), e);
+	std::regex_iterator<std::string::iterator> rend;
+
+	std::cout << line << std::endl;
+	while (rit!=rend) {
+	    std::cout << "Matched: \"" << rit->str() << "\", ";
+	    std::cout << "Position: " << rit->position() << ", ";
+	    std::cout << "Length: " << rit->length() << std::endl;
+	    ++rit;
+	}
     }
-    */
-
     return 0;
 }
