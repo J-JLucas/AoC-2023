1ecf3ce5e02322d948640cd1fbde120d4de821a8
JoSh <jlucas205@proton.me>
2023-12-24 21:18:29 -0700

    cleaned up part2

--- d03/d3p2.cpp
+++ d03/d3p2.cpp
@@ -7,22 +7,14 @@ std::vector<std::string> grid {};   // "engine" grid diagram
 std::vector<std::map<int, std::string>> gearTable{};
 
 int validate(std::regex_iterator<std::string::iterator> it, int lineNum) {
-    // takes in a substring and checks if surrounded
-    // by a validating character in the grid
-/*
-    std::cout << std::endl;
-    std::cout << grid[lineNum-1] << std::endl;
-    std::cout << grid[lineNum] << std::endl;
-    std::cout << grid[lineNum+1] << std::endl;
-    std::cout << it->str() << std::endl;
-*/    
+    // identifies a 'gear' by counting adjacent digit strings
+    // is gear if exactly 2 adjacent strings
+
     int gearRatio = 1;
     int pos = it->position();
     int lbound = pos-1;
     int ubound = lbound+2;
     int matchCount = 0;
-    //std::cout << grid[lineNum] << std::endl;
-    //std::cout << "lbound: " << lbound << " ubound: " << ubound << std::endl;
     std::regex e("(\\d)+");
 
     for (int k = -1; k < 2; k++) {
@@ -33,18 +25,13 @@ int validate(std::regex_iterator<std::string::iterator> it, int lineNum) {
 	while(iter != end) {
 	    int head = iter->position();
 	    int tail = head + iter->length()-1;
-//	    std::cout << "head: " << head << " tail: " << tail << std::endl;
 	    if(!(tail < lbound || head > ubound)) {
-//		std::cout << "inbounds: " << iter->str() << std::endl;
 		gearRatio *= stoi(iter->str());
 		matchCount++;
 	    }
-//	    else {std::cout << "oob" << std::endl;}
 	    iter++;
 	}
     }
-//    std::cout << "matchcount: " << matchCount << std::endl;
-//    std::cout << gearRatio << std::endl;
     if(matchCount == 2 ) { return gearRatio;}
     else {return 0;}
 }
@@ -65,16 +52,14 @@ int main(int argc, char* argv[]) {
     while(getline(std::cin, line)) { line = '.'+ line + '.'; grid.push_back(line); }
     grid.push_back(padding);
     
-    // for each line match all numerical substrings
+    // for each line match all potential "gears"
     std::regex e ("\\*");			
     for (int i = 0; i < grid.size()-1 ; i++) {
 	std::string line = grid[i];
 	std::regex_iterator<std::string::iterator> iter (line.begin(), line.end(), e);
 	std::regex_iterator<std::string::iterator> end;
     
-	//std::cout << line << std::endl;
 	while (iter!=end) {
-	    //std::cout << iter->str() << std::endl;
 	    sum += validate(iter, i);
 	    iter++;
 	}
