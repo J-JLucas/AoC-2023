2349b5e5da9eab7fab145e127df229cf24a2d114
JoSh <jlucas205@proton.me>
2023-12-26 20:13:25 -0700

    testing autoformat and updated email settings

--- d05/d5p1.cpp
+++ d05/d5p1.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-#include <string>
 #include <regex>
+#include <string>
 
-int main(int argc, char* argv[]) {
-
-
-    return 0;
-}
+int main(int argc, char *argv[]) { return 0; }
